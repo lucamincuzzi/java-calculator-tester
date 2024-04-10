bb2ef2965a04e4cb3ebb95dcbe3d068ffdb285c7
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-10 14:47:52 +0200

    add: Calculator class

---
+++ src/main/java/org/experis/learning/java_calculator/Calculator.java
@@ -0,0 +1,20 @@
+package org.experis.learning.java_calculator;
+
+public class Calculator {
+    // Metodi
+    public float add(float num1, float num2){
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2){
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2){
+        return num1 * num2;
+    }
+}
